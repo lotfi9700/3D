773a75d1e7db567b4087fb3ea8e8ad99ea184683
lotfi9700 <livedz547@gmail.com>
2023-05-01 10:09:17 -0700

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import Canvas from "./canvas";
+import Customezer from "./assets/pages/Customezer";
+import Home from "./assets/pages/Home";
+function App() {
+ 
+
+  return (
+ 
+    <main className="app transition-all ease-in">
+      <Home />
+      <Canvas />
+      <Customezer/>
+    </main>
+  )
+}
+
+export default App
